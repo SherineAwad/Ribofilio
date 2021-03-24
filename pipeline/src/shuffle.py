e90d2a77d4cb72555b4ccaea0e4b968614e2b8bf
EC2 Default User <ec2-user@ip-172-31-23-192.eu-west-2.compute.internal>
2021-03-24 10:04:57 +0000

    Update snakefile and shuffle to fix broken latency time lag in shuffle

--- pipeline/src/shuffle.py
+++ pipeline/src/shuffle.py
@@ -7,7 +7,7 @@ import numpy as np
 #------------------------------------------------------------------------
 #profile function: estimates the drop rate of ribosomes after binning
 #------------------------------------------------------------------------
-def shuffle(transcripts,sample,outfile):
+def shuffle(transcripts,sample,outfile,N):
  
     gLength = {} 
 
@@ -17,8 +17,13 @@ def shuffle(transcripts,sample,outfile):
             gname=gname.split('_')[0]
         gLength[gname] = len(record.sequence)
 
-    fp = open(outfile, 'w+')
-    for line in open(sample):
+    files = [] 
+    for i in range(0,N): 
+        fname = outfile +"."+str(i)+".bed" 
+        files.append(fname)
+    for f in files: 
+       fp = open(f, 'w+')
+       for line in open(sample):
             l = line.rstrip().split('\t')
             gname = l[0].strip()
             if "_mRNA" in gname : 
@@ -29,15 +34,17 @@ def shuffle(transcripts,sample,outfile):
             rpos1 = randy[0]
             rpos2 = rpos1 + read_size 
             print (l[0],'\t',str(rpos1),'\t',str(rpos2),'\t',l[3],'\t',l[4],'\t',l[5],file=fp )
+       fp.close() 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-t','--transcripts',dest='transcripts', default=False)
     parser.add_argument('-b','--bed', dest='bed', default=False)
     parser.add_argument('-o', '--outfile', dest='outfile', default=False) 
+    parser.add_argument('-n', dest='N', type= int, default=False) 
     args = parser.parse_args()
 
-    shuffle(args.transcripts, args.bed,args.outfile) 
+    shuffle(args.transcripts, args.bed,args.outfile, args.N) 
 if __name__ == '__main__':
     main()
 
