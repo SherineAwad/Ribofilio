7d332292a5f6bf3d44426348e6d754972c0dc286
EC2 Default User <ec2-user@ip-172-31-23-192.eu-west-2.compute.internal>
2021-05-25 11:43:02 +0000

    Add nox for tests

---
+++ noxfile.py
@@ -0,0 +1,13 @@
+import nox
+
+
+@nox.session
+def tests(session):
+    session.install("-r", "requirements.txt")
+    session.run("pytest", "/home/ec2-user/ribofilio/tests/tests.py")
+
+@nox.session
+def lint(session):
+    session.install("flake8")
+    session.run("flake8", "src/ribofilio.py")
+
