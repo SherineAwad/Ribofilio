a780a04430fde48020f2cf956fdfc6ade1d0dc14
Sherine Awad-Mahmoud <sa941@login-e-12.data.cluster>
2021-08-13 15:39:39 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-     name=‘rtest’,
-     version=‘0.0.0’, 
-     author=‘Sherine Awad’, 
-     author_email='sherine.awad@gmail.com’, 
+     name='rtest',
+     version='0.0.0', 
+     author='Sherine Awad', 
+     author_email='sherine.awad@gmail.com', 
      packages=find_packages(),
 )
