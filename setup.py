da375d54e5e28085930137374629b6f846443166
Sherine Awad-Mahmoud <sa941@login-e-12.data.cluster>
2021-08-13 15:28:03 +0100

    update setup.cfg, meta.yaml, bld.bat, build.sh

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setup tools import setup, find_packages
+
+setup(
+     name=‘rtest’,
+     version=‘0.0’, 
+     author=‘Sherine Awad’, 
+     author_email='sherine.awad@gmail.com’, 
+     packages=find_packages(),
+)
