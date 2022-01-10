12a712f8103d4a8090f96d6b31a46cdca77f4b42
Sherine Awad-Mahmoud <sa941@login-e-11.data.cluster>
2022-01-10 09:32:18 +0000

    update ttest variable name, 2sided pv in ttest and ribofilio.py

--- pipeline/scripts/ttest.py
+++ pipeline/scripts/ttest.py
@@ -18,16 +18,16 @@ def getstats(infile1, infile2):
         if count == 0: 
             count+=1 
             continue
-        slope1, _, _, _, SE1, _, _, _, n1 = line.split('\t')
+        dr1, _, _, _, SE1, _, _, _, n1 = line.split('\t')
     count = 0
     for line in open(infile2): 
         if count == 0:
              count+=1 
              continue
-        slope2, _, _, _, SE2, _, _, _, n2 = line.split('\t') 
+        dr2, _, _, _, SE2, _, _, _, n2 = line.split('\t') 
     df = ( float(n1) + float(n2) )-4
-    tscore = (float(slope1) - float(slope2)) / np.sqrt(np.square(float(SE1)) +np.square(float(SE2)) ) 
-    pvalue =  (t.sf(abs(tscore),df= df))
+    tscore = (float(dr1) - float(dr2)) / np.sqrt(np.square(float(SE1)) +np.square(float(SE2)) ) 
+    pvalue =  2 *  (t.sf(abs(tscore),df= df))
     pvalue = np.round(pvalue, decimals=4)
     print(infile1,'\t', infile2)
     print("tscore",'\t', "pvalue")
